57a4b08b3613fa869d4663cd33b48310fce1f209
jacketzhang2000 <2323038395@qq.com>
2019-05-16 23:11:19 +0800

    java

---
+++ 1.java
@@ -0,0 +1,10 @@
+Public class helloworldApp {
+	public static void main (String[] args) {
+		System.out.println("你好中国");
+		for (int i=1; i<10;i++)  {
+			for (int j=1; j<10;j++)  {
+			System.out.println(j*i);
+			}
+		}
+	    }
+}

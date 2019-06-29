a01d8041885aa5cd42f586e38ce2559941d10cfb
jacketzhang2000 <2323038395@qq.com>
2019-06-29 08:08:39 +0800

    提交代码

---
+++ learnStringAdd.java
@@ -0,0 +1,11 @@
+public class learnStringAdd{
+public static void learnStringAdd() {
+int a=3;
+int b=4;
+System.out.println("结果是"+a+b);
+System.out.println("结果是"+(a+b));
+}
+public static void main(String[] args) {
+learnStringAdd();
+}
+}

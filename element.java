2673597afa9e9564161e8eed3dd1cbab0d05cab0
jacketzhang2000 <2323038395@qq.com>
2019-06-30 00:00:58 +0800

    提交代码

---
+++ element.java
@@ -0,0 +1,35 @@
+
+public class element{
+	public int index;
+	public int element;
+	public int size;
+	//中间插入
+	public void insert (int index,int element){
+	//先判断插入合法[0-size-1]
+		if（index
+		
+		for (int i=size;i>index,i--){
+			array[index]=element;//i空间的下标     数据：【i-1】
+			size++;
+			/*for (int i=0;i<size-index,i++){
+			array[size-i]=array[size-i-1];//
+			size++;
+			头插：o(n)
+			尾插：o(1)
+			中间：o(n)
+			*/
+			
+	}
+	//头删
+	public void popFront(){
+		for(int i=0;i<ize-1;i++){
+		array[i]=array[i+1];
+		}
+	}
+
+	
+	
+	public static void main (String[] args){
+		
+	}
+}
